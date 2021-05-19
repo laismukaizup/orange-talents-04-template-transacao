41c9d158508d52af5d38a027b3211121d37a0f13
Lais Mukai <lais.mukai@zup.com.br>
2021-05-19 14:46:50 -0300

    setup do projeto

---
+++ src/test/java/br/com/zup/academy/lais/Transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.academy.lais.Transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
